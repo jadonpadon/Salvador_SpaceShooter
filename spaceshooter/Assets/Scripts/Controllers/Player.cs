accdd6f512a7ce8ed9095c98ac2ab2c3c4ac5c5b
jadonpadon <salvador.jaden@gmail.com>
2024-10-10 02:15:29 -0400

    task 1c incomplete
    its 2am i need sleep

--- spaceshooter/Assets/Scripts/Controllers/Player.cs
+++ spaceshooter/Assets/Scripts/Controllers/Player.cs
@@ -9,6 +9,11 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    public float maxSpeed;
+    public float accelerationTime;
+    public float decelerationTime;
+    float speed;
+
     void Update()
     {
         PlayerMovement();
@@ -16,27 +21,67 @@ public class Player : MonoBehaviour
 
     void PlayerMovement()
     {
-        Vector3 upVel = new Vector3(0, 1);
-        Vector3 downVel = new Vector3(0, -1);
-        Vector3 leftVel = new Vector3(-1, 0);
-        Vector3 rightVel = new Vector3(1, 0);
+        float acceleration = maxSpeed / accelerationTime;
+
+        if (speed > maxSpeed )
+        {
+            speed = maxSpeed;
+        }
+        if (speed < 0)
+        {
+            speed = 0;
+        }
+
+        Vector3 upVel = new Vector3(0, speed);
+        Vector3 downVel = new Vector3(0, -speed);
+        Vector3 leftVel = new Vector3(-speed, 0);
+        Vector3 rightVel = new Vector3(speed, 0);
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
+            speed += acceleration * Time.deltaTime;
             transform.position += upVel *Time.deltaTime;
         }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            speed -= decelerationTime * Time.deltaTime;
+            transform.position += upVel * Time.deltaTime;
+        }
+
         if (Input.GetKey(KeyCode.DownArrow))
         {
+            speed += acceleration * Time.deltaTime;
             transform.position += downVel * Time.deltaTime;
         }
+        if(Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            speed -= decelerationTime * Time.deltaTime;
+            transform.position += downVel * Time.deltaTime;
+        }
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            speed += acceleration * Time.deltaTime;
             transform.position += leftVel * Time.deltaTime;
         }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            speed -= decelerationTime * Time.deltaTime;
+            transform.position += leftVel * Time.deltaTime;
+        }
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            speed += acceleration * Time.deltaTime;
             transform.position += rightVel * Time.deltaTime;
         }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            speed -= decelerationTime * Time.deltaTime;
+            transform.position += rightVel * Time.deltaTime;
+        }
+
+        Debug.Log(speed);
     }
 
 }
